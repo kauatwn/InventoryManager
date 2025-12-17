83bbbe24a32e160dd5cf5b897a0447da1d3716d7
kauatwn <kaualopescontato@gmail.com>
2025-12-17 14:31:11 -0300

    chore: exclude simple exceptions from code coverage

--- src/InventoryManager.Domain/Exceptions/ConflictException.cs
+++ src/InventoryManager.Domain/Exceptions/ConflictException.cs
@@ -1,3 +1,6 @@
-﻿namespace InventoryManager.Domain.Exceptions;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace InventoryManager.Domain.Exceptions;
+
+[ExcludeFromCodeCoverage]
 public sealed class ConflictException(string message) : Exception(message);
--- src/InventoryManager.Domain/Exceptions/NotFoundException.cs
+++ src/InventoryManager.Domain/Exceptions/NotFoundException.cs
@@ -1,3 +1,6 @@
-﻿namespace InventoryManager.Domain.Exceptions;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace InventoryManager.Domain.Exceptions;
+
+[ExcludeFromCodeCoverage]
 public sealed class NotFoundException(string message) : Exception(message);
