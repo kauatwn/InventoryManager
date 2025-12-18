a65b4ed713a4a35c1be430f1a693d4f3c588a231
kauatwn <kaualopescontato@gmail.com>
2025-12-18 13:02:23 -0300

    feat: implement GetProductById use case and tests

---
+++ src/InventoryManager.Application/UseCases/Products/GetById/GetProductByIdUseCase.cs
@@ -0,0 +1,33 @@
+﻿using InventoryManager.Application.DTOs.Responses;
+using InventoryManager.Domain.Entities;
+using InventoryManager.Domain.Exceptions;
+using InventoryManager.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryManager.Application.UseCases.Products.GetById;
+
+public sealed partial class GetProductByIdUseCase(
+    IProductRepository repository,
+    ILogger<GetProductByIdUseCase> logger) : IGetProductByIdUseCase
+{
+    public const string ProductNotFoundMessage = "Product with Id '{0}' not found.";
+
+    public ProductResponse Execute(Guid id)
+    {
+        LogExecution(id);
+
+        Product product = repository.GetById(id) ?? throw new NotFoundException(string.Format(ProductNotFoundMessage, id));
+
+        return new ProductResponse(
+            product.Id,
+            product.Name,
+            product.Description,
+            product.Price,
+            product.StockQuantity,
+            product.Sku
+        );
+    }
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Fetching product with Id: {Id}")]
+    private partial void LogExecution(Guid id);
+}
---
+++ src/InventoryManager.Application/UseCases/Products/GetById/IGetProductByIdUseCase.cs
@@ -0,0 +1,8 @@
+﻿using InventoryManager.Application.DTOs.Responses;
+
+namespace InventoryManager.Application.UseCases.Products.GetById;
+
+public interface IGetProductByIdUseCase
+{
+    ProductResponse Execute(Guid id);
+}
