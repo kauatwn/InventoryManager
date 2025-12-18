a99cbcb41101a6a33f551934784a412835b4d72c
kauatwn <kaualopescontato@gmail.com>
2025-12-18 12:57:08 -0300

    feat: add Create Product use case, validation, and tests

---
+++ src/InventoryManager.Application/UseCases/Products/Create/ICreateProductUseCase.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManager.Application.DTOs.Requests;
+using InventoryManager.Application.DTOs.Responses;
+
+namespace InventoryManager.Application.UseCases.Products.Create;
+
+public interface ICreateProductUseCase
+{
+    ProductResponse Execute(CreateProductRequest request);
+}
