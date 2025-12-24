f631292cb0ea14089fd38d74a81cdc2190f797f6
kauatwn <kaualopescontato@gmail.com>
2025-12-24 18:18:52 -0300

    feat: add InventoryDbContext

---
+++ src/InventoryManager.Infrastructure/Persistence/InventoryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManager.Infrastructure.Persistence;
+
+public sealed class InventoryDbContext(DbContextOptions<InventoryDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(InventoryDbContext).Assembly);
+    }
+}
