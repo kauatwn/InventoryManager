b1df145d6d62d2eb602a1a6855260d1fa719c726
kauatwn <kaualopescontato@gmail.com>
2025-12-24 21:50:32 -0300

    feat: add database initialization extension method

---
+++ src/InventoryManager.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManager.Infrastructure.Persistence;
+using System.Diagnostics.CodeAnalysis;
+
+namespace InventoryManager.API.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "Infrastructure wrapper for database initialization")]
+public static class DatabaseExtensions
+{
+    public static async Task CreateDatabaseAsync(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<InventoryDbContext>();
+
+        await context.Database.EnsureCreatedAsync();
+    }
+}
