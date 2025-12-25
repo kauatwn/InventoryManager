36e10a67f9998033e72f530cedb91e0506c53fc7
kauatwn <kaualopescontato@gmail.com>
2025-12-24 21:50:06 -0300

    feat: add exception handling extensions for API

---
+++ src/InventoryManager.API/Extensions/ExceptionHandlingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManager.API.Infrastructure.Errors;
+using System.Diagnostics.CodeAnalysis;
+
+namespace InventoryManager.API.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "Pure dependency injection configuration")]
+public static class ExceptionHandlingExtensions
+{
+    public static IServiceCollection AddApiExceptionHandlers(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<ValidationExceptionHandler>();
+
+        services.AddExceptionHandler<DomainExceptionHandler>();
+        services.AddExceptionHandler<NotFoundExceptionHandler>();
+        services.AddExceptionHandler<ConflictExceptionHandler>();
+
+        services.AddExceptionHandler<UnhandledExceptionHandler>();
+        services.AddProblemDetails();
+
+        return services;
+    }
+}
